580ece9cbd45d15c623c8a171cd2d8f2dc8f8ede
Vihaan Akshaay Rajendiran <44992406+VihaanAkshaay@users.noreply.github.com>
2020-03-27 08:35:12 +0530

    Update problem_6.py

--- problem_6.py
+++ problem_6.py
@@ -19,9 +19,32 @@ def get_min_max(ints):
     return (min,max)
 
 
-## Example Test Case of Ten Integers
-import random
-
+#%% Testing - Official
+# Normal cases
+print('Normal Cases:')
+# Case 1
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
+# Case 2
+l = [i for i in range(-12, 25)]  # a list containing -12 - 24
+random.shuffle(l)
+print("Pass" if ((-12, 24) == get_min_max(l)) else "Fail")
+print('\n')
+
+# Edge cases
+print('Edge Cases:')
+# Case 3
+l = [i for i in range(300, 301)]  # a list containing 300
+random.shuffle(l)
+print("Pass" if ((300, 300) == get_min_max(l)) else "Fail")
+
+# Case 4
+l = []  # an empty list
+print("Pass" if (None == get_min_max(l)) else "Fail")
+
+# Case 5
+l = [i for i in range(-24, -1)]  # a list containing -24 - -2
+random.shuffle(l)
+print("Pass" if ((-24, -2) == get_min_max(l)) else "Fail")
